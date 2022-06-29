5244df1116184d4dc633ba7e3a8cc92cfc61ca33
gabriel <Gabrielvasco906@gmail.com>
2022-06-29 16:25:28 -0300

    usando o calcula imc

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,16 @@
 var titulo = document.querySelector(".titulo")
-
 titulo.textContent = 'Aparecida Nutricionista';
+
+let paulo = document.querySelector("#paulo");
+let tdPeso = document.querySelector(".info-peso");
+
+const peso = tdPeso.textContent;
+
+let tdAltura = paulo.querySelector(".info-altura");
+let altura = tdAltura.textContent;
+
+const tdImc = paulo.querySelector(".info-imc");
+
+let imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
