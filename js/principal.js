785d1971c10d9f344c40506d212550d537234e44
gabriel <Gabrielvasco906@gmail.com>
2022-06-30 17:30:34 -0300

    Operados logicos

--- js/principal.js
+++ js/principal.js
@@ -11,6 +11,21 @@ let altura = tdAltura.textContent;
 
 const tdImc = paulo.querySelector(".info-imc");
 
-let imc = peso / (altura * altura);
+let pesoValido = true;
+let alturaValida = true;
 
-tdImc.textContent = imc;
+if(peso <= 0 || peso >= 1000) {
+    console.log('peso invalido');
+    pesoValido = false;
+    tdImc.textContent = "Peso invalido";
+} 
+
+if (altura <= 0 || altura >= 4.00){
+    console.log('altura invalida');
+    alturaValida = false;
+    tdAltura.textContent = "Altura invalida"
+}
+if (alturaValida && pesoValido){
+    let imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
