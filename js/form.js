c036522ffc2996e3a4b2b3b393a685f0009d4f1f
gabriel <Gabrielvasco906@gmail.com>
2022-07-10 21:01:53 -0300

    Capturando erros de requisição

--- js/form.js
+++ js/form.js
@@ -15,17 +15,20 @@ botaoAdicionar.addEventListener("click", function(event) {
 
         return;
     }
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
+    
+    adicionaPacienteNaTabela(paciente)
 
     form.reset();
-
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
-
 });
+
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function obtemPacienteDoFormulario(form) {
 
